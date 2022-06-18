95aa301696f4ce14d7ed993cf7b439a7529c34ca
Sydney McGee <mcgee.sydney@gmail.com>
2022-06-18 12:47:14 -0500

    Registered product model with admin site + complete POST functionalitiy for Product + update REST Framework decimal settings

--- products/views.py
+++ products/views.py
@@ -1,10 +1,24 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ProductsSerializer
+from .models import Products
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def products_list(request):
 
+    if request.method == 'GET':
+        products = Products.objects.all()
+        serializer = ProductsSerializer(products, many=True)
+        return Response(serializer.data)
 
-    return Response('ok')
+    elif request.method == 'POST':
+        serializer = ProductsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+       
+       
+
 
